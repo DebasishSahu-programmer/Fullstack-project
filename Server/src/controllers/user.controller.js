925e13b5cbf142789eb0bb24fbd01dde4ec37acf
Debasish Sahu <debasishsahu5901@gmail.com>
2026-08-30 21:27:49 +0530

    fix: update cookie options to include sameSite attribute for enhanced security

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -110,7 +110,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none"
     }
 
     return res
@@ -144,7 +145,8 @@ const logoutUser = asyncHandler(async (req, res) => {
     )
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none"
     }
 
     return res
@@ -182,7 +184,8 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
         const options = {
             httpOnly: true,
-            secure: true
+            secure: true,
+            sameSite: "none"
         }
 
         return res
